3dc5e985701ed0fc6dc76ef9da4258afd933e576
Kristine Fernandez <hello@kristinefernandez.com>
2023-03-27 07:14:00 -0500

    progress + stop motion

--- video/sketch.js
+++ video/sketch.js
@@ -39,22 +39,41 @@ let offset = 0, limit = 4;
 function mousePressed(){
   sound.loop();
 }
-function draw(){
-  
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+}
+
+function draw() {
+  //background(220);
+  let exponent = 3;
+  let squareSize = 20;
+  for (let i = 0; i < width; i += squareSize) {
+    let numSquares = floor(pow(random(0, 1), exponent) * 4);
+    for (let j = 0; j < numSquares; j++) {
+      let x = i + (random(0, 1) * squareSize) - width/2;
+      let y = (height - (numSquares * squareSize)) + (j * squareSize) - height/2;
+      fill(random(255), random(255), random(255));
+      rect(x, y, squareSize, squareSize);
+    }
+  }
+
+
   currTime++;
   
-  shader(theShader);
+  // shader(theShader);
 
 
-  //const percentageY = map(mouseY, - height/2, height/2, 0, 1);
-  const percentageX = map(mouseX, -width/2, width/2, 0, 1);
-  const rate = lerp(0.9, 1.2, percentageX);
-  sound.rate(rate);
-
-  
-  theShader.setUniform('tex0', bgVideo);
+  // //const percentageY = map(mouseY, - height/2, height/2, 0, 1);
+  // const percentageX = map(mouseX, -width/2, width/2, 0, 1);
+  // const rate = lerp(0.9, 1.2, percentageX);
+  // sound.rate(rate);
 
   
-  rect(0,0,width, height);
+  // theShader.setUniform('tex0', bgVideo);
+
+  
+  // rect(0,0,width, height);
 }
  
